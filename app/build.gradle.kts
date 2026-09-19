934d5a49a73640590f3cc71d2440460fe4353799
kwb130212-discord <kwb130212@gmail.com>
2026-09-19 21:40:55 +0900

    feat: add MacroV1 Android automation app (app/build.gradle.kts)

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.kwb130212.macrov1"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.kwb130212.macrov1"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 10
+        versionName = "1.0"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
